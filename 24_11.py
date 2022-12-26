e51165a2618e309ed37f9e96f998593c1c3200d7
LeraKrylova <113025441+LeraKrylova@users.noreply.github.com>
2022-12-26 21:44:38 +0300

    Create 24_11.py

---
+++ 24_11.py
@@ -0,0 +1,68 @@
+class NotValidFigure(Exception):
+    pass
+
+import csv 
+
+class Building:
+    def __init__(self, floors: int, height: int, width: int, name: str):
+        self.floors = floors #этажи
+        self.height = height #высота
+        self.width = width #ширина
+        self.name = name #название 
+        if not self.is_valid():
+            raise NotValidFigure('Коректно введите данные')
+
+    def is_valid(self):
+        buil_ = self.floors, self.height, self.width 
+        if all([float(bui) > 0 for bui in buil_]):
+            return True
+        return False
+
+    @classmethod
+    def from_tuple(cls, data):
+        return cls(*data)
+
+    def __str__(self):
+        return 'этажи : {0}, высота : {1}, ширина : {2}, название : {3}'.format(self.floors, self.height, self.width, self.name)
+
+    def to_tuple(self):
+        return self.floors, self.height, self.width, self.name
+
+
+def bul(building):
+    with open('domic', 'a') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(building.to_tuple())
+    print('Данные о здание {}, успешно добавлены в файл'.format(n_))
+
+def read():
+    with open('domic', 'rt') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            print(Building.from_tuple(row))
+
+f = 'Введите количество зданий:'
+h = 'Введите высоту здания:'
+w = 'Введите ширину здания:'
+n = 'Введите название здания:'
+
+print('Вы будете записывать данные о здании или прочитать? 0/1')
+d  = input()
+if d == '0':
+    try:
+        f_, h_, w_, n_ =  int(input(f, )), int(
+            input(h )), int(input(w, )), input(n, )
+    except TypeError:
+        print('Неправильно ввели данные, попробуйте еще раз!')
+    building = Building(f_, h_, w_, n_)
+    bul(building)
+elif d == '1':
+    read()
+
+
+
+
+
+
+
+
